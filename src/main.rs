5052957c89a3c159c315e2553510706e8e873486
Loïc Girault <lgirault@mediarithmics.com>
2019-11-02 00:50:00 +0100

    tmp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+
+//enum Frame {
+//    Data(),
+//
+//}
+
+enum Method {
+    Get,
+    Put,
+    Post,
+    Delete,
+    Option,
+}
+impl Method {
+    fn from_string(s : &str) -> Option<Method> {
+
+        match s.to_lowercase().as_str() {
+            "get" => Some(Method::Get),
+            "put" => Some(Method::Put),
+            "post" => Some(Method::Post),
+            "delete" => Some(Method::Delete),
+            "option" => Some(Method::Option),
+            _ => None,
+        }
+//        if s.to_lowercase() == "get" { Some(Method::Get)}
+//        else {None}
+    }
+}
+enum Protocol {
+    _11,
+    _2
+}
+
+struct Request {
+    method: Method,
+    path: String,
+    protocol: Protocol,
+
+}
+
+impl Request {
+
+    fn from_string(s: &str) -> Result<Request, &'static str> {
+        let mut token = s.split(' ');
+        let method = token.next().and_then(|s| Method::from_string(s));
+
+        Err("not implemented")
+    }
+
+}
+
 fn main() {
     println!("Hello, world!");
 }
